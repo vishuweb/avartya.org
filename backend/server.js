4bad33f888bde2a1d6b48a3744caacf8f618398d
Vishal <vishalkannaujiya230310558313@gmail.com>
2026-03-17 16:52:36 +0530

    fixed PORT for deployment

--- backend/server.js
+++ backend/server.js
@@ -28,7 +28,7 @@ app.get("/", (req, res) => {
   res.send("Avartya Backend Running");
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
